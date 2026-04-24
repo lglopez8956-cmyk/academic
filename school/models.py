683cf0ad52ad37688c281e25d0e387871835b5e6
lglopez8956-cmyk <lglopez.8956@unicesmag.edu.co>
2026-04-24 16:17:49 -0500

    Comentario Nuevo

--- school/models.py
+++ school/models.py
@@ -4,3 +4,5 @@ class User(models.Model):
     email = models.CharField(max_length=100)
     password = models.TextField(max_length=500)
     status = models.BooleanField(default=True)
+
+    # Cambio de prueba
